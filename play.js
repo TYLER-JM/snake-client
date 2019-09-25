a82a8623a972fea9c2d8d71a3654b66379d6a76d
Tyler <t.mar92@gmail.com>
2019-09-25 16:52:02 +0000

    first implementation of function

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '172.46.2.204',
+    port: 50541
+  });
+  // conn.on("close", () => {
+  //   console.log("you ded cuz you idled");
+  // });
+  conn.on("data", (data) => {
+    console.log("message from client: ", data);
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
